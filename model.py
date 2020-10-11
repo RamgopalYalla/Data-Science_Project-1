d70b259b32d6a1f9205b8497901cc3b144ff656b
Ramgopal Yalla <67060501+RamgopalYalla@users.noreply.github.com>
2020-10-11 20:30:57 +0200

    inporting required packages

---
+++ model.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+# package for algebra calculations
+import numpy as np
+
+# package for processing the data
+import pandas as pd
+
+# pakage for data visualization
+import seaborn as sns
+
+# matplot libraries
+from matplotlib import pyplot as plt
+from matplotlib import style
+
+# algorithms and models
+from sklearn import linear_model
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import SGDClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC, LinearSVC
+from sklearn.naive_bayes import GaussianNB
+
+
+# In[10]:
+
+
+test_df = pd.read_csv("test.csv")
+train_df = pd.read_csv("train.csv")
+
+
+# In[11]:
+
+
+train_df.info()
+
+
+# In[19]:
+
+
+survival:    Survival 
+PassengerId: Unique Id of a passenger. 
+pclass:    Ticket class     
+sex:    Sex     
+Age:    Age in years     
+sibsp:    # of siblings / spouses aboard the Titanic     
+parch:    # of parents / children aboard the Titanic     
+ticket:    Ticket number     
+fare:    Passenger fare     
+cabin:    Cabin number     
+embarked:    Port of Embarkation
+    
+    
+train_df.describe()
+
+
+# In[ ]:
+
+
+
+
