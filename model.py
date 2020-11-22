c1b50e4323f90707582d2b87ef82be3a97e871c3
RamgopalYalla <ramgopal678@gmail.com>
2020-11-22 16:49:23 +0100

    update 22112020

--- model.py
+++ model.py
@@ -54,7 +54,7 @@ sibsp:    # of siblings / spouses aboard the Titanic
 parch:    # of parents / children aboard the Titanic     
 ticket:    Ticket number     
 fare:    Passenger fare     
-cabin:    Cabin number     
+
 
     
 train_df.describe()
